24c81049bb4764973798222b90b233a1ac12d879
Dawn <dawn.mott@oregonmetro.gov>
2019-11-18 13:13:28 -0800

    update css to scss

--- app/routes/production.js
+++ app/routes/production.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
+import hasEmberVersion from 'ember-test-helpers/has-ember-version';
 
 export default Route.extend({
+  model() {
+    // return {
+    //   "ProductName": "Poderosa",
+    //   "DimensionName": "1\" X 4\" X 14'",
+    //   "BoardsSum": 20,
+    //   "BoardFeetSum": 10000
+    // }
+    return hasEmberVersion.$.getJSON("/data/production.json");
+  }
 });
